b03bac0d55a9e4fd61fb35c91abffa59b3f65df0
blitzdawolf <botjes2@hotmail.com>
2020-01-05 14:20:56 +0100

    Added example project

---
+++ MerpEngineExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using MerpEngine;
+using System;
+
+namespace MerpEngineExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Level l = new Level();
+            Debug.Log(l.Save());
+
+            Game.Start();
+        }
+    }
+}
