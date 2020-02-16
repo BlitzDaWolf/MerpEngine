df4f607a22b67bdd7fd436e8f3e7e8e0fd1f2e95
blitzdawolf <botjes2@hotmail.com>
2020-02-17 00:32:49 +0100

    Added global physics

---
+++ MerpEngine.Physics/src/GlobalPhysics.cs
@@ -0,0 +1,12 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MerpEngine.Physics
+{
+    public class GlobalPhysics
+    {
+        public static Vector2 Gravity = new Vector2(0, -9.81f);
+    }
+}
